c9af340536ba6cb2d403705e3f171b1f2493d9e5
Frantisek Troster <frantisek.troster@gmail.com>
2014-07-22 15:45:57 +0200

    Initial commit yo backbone

---
+++ app/scripts/main.coffee
@@ -0,0 +1,12 @@
+window.A18Client =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  init: ->
+    'use strict'
+    console.log 'Hello from Backbone!'
+
+$ ->
+  'use strict'
+  A18Client.init();
---
+++ test/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global A18Client, $*/
+
+
+window.A18Client = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    A18Client.init();
+});
